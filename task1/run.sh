519cae4b8618388de015ed70f6d07b63d2bd07ad
stepyndriyy <56322857+stepyndriyy@users.noreply.github.com>
2020-02-25 13:51:45 +0300

    Update run.sh

--- task1/run.sh
+++ task1/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 chmod +x run.sh
 mkdir $1/$3
-find $1 -name "*.$2" -exec cp --backup=t -- {} $1/$3/ \;
+find $1 -name "*.$2" -exec cp --backup=t {} $1/$3/ \;
 tar -czf $1/$4 $1/$3
 rm -r $1/$3
 echo 'done'
