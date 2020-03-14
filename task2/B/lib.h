46a6d2cdd95fa21cd50ef2a9e2330909692fa963
stepan <stepyndriyy@gmail.com>
2020-03-14 21:44:51 +0300

    done easy pass

---
+++ task2/B/lib.h
@@ -0,0 +1,12 @@
+#pragma once
+
+class Primitive { 
+private:
+    int a, b;
+public:
+    Primitive() : a(0), b(0) {}
+    Primitive(int _a, int _b) : a(_a), b(_b) {}
+
+    int sum();
+    int mult();
+};
---
+++ task2/C/main.cpp
@@ -0,0 +1,8 @@
+#include "main.h"
+//#include<iostream>
+
+int main() {
+    int a;
+    std::cout << " porabotai v postele beiba\n";
+
+}
