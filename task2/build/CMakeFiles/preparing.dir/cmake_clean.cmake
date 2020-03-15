8b85a8d7ad45394a12a218a1745821542547c76a
stepan <stepyndriyy@gmail.com>
2020-03-15 22:34:17 +0300

    5555555

--- task2/build/CMakeFiles/preparing.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/preparing"
-  "../A/index.h"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/preparing.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
